cbc78542b316d3f2e119b7730a57448b2a1c6efd
Vitali Judin <65638870+VitaliJud@users.noreply.github.com>
2023-06-06 01:18:04 -0500

    Add files via upload

---
+++ customer-segment-manager-main/lib/hooks.ts
@@ -0,0 +1,110 @@
+import useSWR from 'swr';
+import { useSession } from '../context/session';
+import { ErrorProps, ListItem, Order, QueryParams } from '../types';
+
+async function fetcher(url: string, query: string) {
+    const res = await fetch(`${url}?${query}`);
+
+    // If the status code is not in the range 200-299, throw an error
+    if (!res.ok) {
+        const { message } = await res.json();
+        const error: ErrorProps = new Error(message || 'An error occurred while fetching the data.');
+        error.status = res.status; // e.g. 500
+        throw error;
+    }
+
+    return res.json();
+}
+
+// Reusable SWR hooks
+// https://swr.vercel.app/
+export function useProducts() {
+    const { context } = useSession();
+    const params = new URLSearchParams({ context }).toString();
+    // Request is deduped and cached; Can be shared across components
+    const { data, error } = useSWR(context ? ['/api/products', params] : null, fetcher);
+
+    return {
+        summary: data,
+        isLoading: !data && !error,
+        error,
+    };
+}
+
+export function useProductList(query?: QueryParams) {
+    const { context } = useSession();
+    const params = new URLSearchParams({ ...query, context }).toString();
+
+    // Use an array to send multiple arguments to fetcher
+    const { data, error, mutate: mutateList } = useSWR(context ? ['/api/products/list', params] : null, fetcher);
+
+    return {
+        list: data?.data,
+        meta: data?.meta,
+        isLoading: !data && !error,
+        error,
+        mutateList,
+    };
+}
+
+export function useProductInfo(pid: number, list: ListItem[]) {
+    const { context } = useSession();
+    const params = new URLSearchParams({ context }).toString();
+    const product = list.find(item => item.id === pid);
+    // Conditionally fetch product if it doesn't exist in the list (e.g. deep linking)
+    const { data, error } = useSWR(!product && context ? [`/api/products/${pid}`, params] : null, fetcher);
+
+    return {
+        product: product ?? data,
+        isLoading: product ? false : (!data && !error),
+        error,
+    };
+}
+
+export const useOrder = (orderId: number) => {
+    const { context } = useSession();
+    const params = new URLSearchParams({ context }).toString();
+    const shouldFetch = context && orderId !== undefined;
+
+    // Conditionally fetch orderId is defined
+    const { data, error } = useSWR<Order, ErrorProps>(shouldFetch ? [`/api/orders/${orderId}`, params] : null, fetcher);
+
+    return {
+        order: data,
+        isLoading: !data && !error,
+        error,
+    };
+}
+
+export const useCustomer = (customerId: string) => {
+    const { context } = useSession();
+    const params = new URLSearchParams({ context, "id:in": customerId}).toString();
+    const shouldFetch = context && customerId !== undefined;
+
+    // Conditionally fetch orderId is defined
+    const { data, error, mutate: mutateCustomer } = useSWR(shouldFetch ? [`/api/customers`, params] : null, fetcher);
+    
+    return {
+        customer: data?.data[0],
+        customerMeta: data?.meta,
+        customerLoading: !data && !error,
+        customerError: error,
+        mutateCustomer
+    };
+}
+
+export function useSegments(query?: QueryParams) {
+    const { context } = useSession();
+    const params = new URLSearchParams({ ...query, context }).toString();
+
+    // Use an array to send multiple arguments to fetcher
+    const { data, error, mutate: mutateSegments } = useSWR(context ? ['/api/segments', params] : null, fetcher);
+    
+    return {
+        segments: data?.data,
+        segmentMeta: data?.meta,
+        segmentsLoading: !data && !error,
+        segmentError: error,
+        mutateSegments,
+    };
+}
